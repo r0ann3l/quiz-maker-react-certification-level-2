ccf9da6f3533639350c47bcbab5634959046092d
Roannel <16123086+r0ann3l@users.noreply.github.com>
2024-10-01 23:15:01 +0200

    Added a basic loading indicator

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import Select from "./Select";
 import useCategoryRequest from "../hooks/useCategoryRequest";
+import LoadingIndicator from "./LoadingIndicator";
 
 const CategorySelect = ({ selected, onSelect }) => {
 
-  const { result } = useCategoryRequest();
+  const { result, loading } = useCategoryRequest();
 
   const options = result.map(option => ({
     value: option.id,
@@ -12,11 +13,13 @@ const CategorySelect = ({ selected, onSelect }) => {
   }));
 
   return (
-    <Select
-      onSelect={onSelect}
-      options={options}
-      selected={selected}
-    />
+    loading ?
+      <LoadingIndicator /> :
+      <Select
+        onSelect={onSelect}
+        options={options}
+        selected={selected}
+      />
   );
 }
 
---
+++ src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+const LoadingIndicator = () => {
+  return (
+    <>
+      Loading....
+    </>
+  );
+}
+
+export default LoadingIndicator;
